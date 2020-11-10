1a7cb082cd831647bde7ccc78109aaf01b7bd740
leecrab <dnddlup929@naver.com>
2020-11-10 12:05:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,10 @@
-def solution(m, n, puddles):
-    dp = [[1]*m for _i in range(n)]
-    
-    for x,y in puddles:
-        dp[y-1][x-1] = 0
-        if x-1 == 0:
-            for k in range(y-1,n):
-                dp[k][0]=0
-        if y-1 == 0:
-            for k in range(x-1,m):
-                dp[0][k]=0
+# 프로그래머스 멀리뛰기
 
-    for i in range(1,n):
-        for j in range(1,m):
-            if i*j != 0:
-                if dp[i][j] != 0:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
-    answer = dp[n-1][m-1]
-    return answer%1000000007
-
-
-
-#print(solution(4,3,[[2,2]]))
-
-print(solution(5,4,[[3,1]]))
-
-[[1, 1, 0, 0, 0], 
-[1, 2, 2, 2, 2], 
-[0, 2, 4, 6, 8], 
-[1, 3, 7, 13, 21]]
-
-
+def solution(n):
+    answer = 0
+    dp = [0 for i in range(n)]
+    dp[0] = 1
+    dp[1] = 2
+    for i in range(2,n):
+        dp[i] = (dp[i-1] +dp[i-2])
+    return dp[n-1]
